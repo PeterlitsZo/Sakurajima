7c33438673453e672897626435c334ea1d339f4c
peterlits zo <peterlitszo@outlook.com>
2019-08-01 13:43:14 +0800

    Use the text bar and \r~ and also make 1 file into 2 files.

---
+++ lib/SakurajimaLIB.py
@@ -0,0 +1,56 @@
+#hash:used to get MD5 list
+#os:used to get the size of file (to show the bar)
+#threading:used to show the bar
+import hashlib
+import os
+import threading
+import math
+
+theMD5_of_Peterlits = \
+    b'\xa2\x44\xd2\xc5\x45\x70\x7d\x00\x84\x13\x82\x2a\xa3\x74\xcf\xab'
+file_flag = [0]
+
+def The_Number_Of_Bit_Of_1 (StringOf16):
+    theDict = {"0":0,"1":1,"2":1,"3":2,"4":1,"5":2,"6":2,"7":3,\
+               "8":1,"9":2,"a":2,"b":3,"c":2,"d":3,"e":3,"f":4}
+    theNumber = 0
+    for i in StringOf16:
+        theNumber += theDict[i]
+    return theNumber
+
+def MixTwoByte (Byte1, Byte2):
+    return Byte1^Byte2
+
+def Mix_Two_Bytes (Bytes1, Bytes2):
+    MixedBytes = []
+    for i in range(Bytes1.__len__()):
+        MixedBytes.append (MixTwoByte (Bytes1[i], Bytes2[i]))
+    return bytes(MixedBytes)
+
+def Negate_of_Bytes (Bytes):
+    AimBytes = []
+    for Byte in Bytes:
+        AimBytes.append (~Byte)
+    return bytes(AimBytes)
+
+def Down_To_Next_Int(num):
+    return math.ceil(num)-1;
+
+def Bit_Of_Num(num, bit):
+    if type(num)='float':
+        num=Down_To_Next_Int(num)
+    return num//(10**(i-1))-num//(10**i)*10
+
+def Write_Bar (long, fillchar, times, endchar='\n'):
+    for i in range(long):
+        print (fillchar*times, sep='',end='')
+    print(endchar,sep='',end='')
+
+def Bars (flag, _len)
+    while True:
+        rate = flag[0] / _len * 100
+        print("%-12.6f%%"%rate ,sep='|',end='')
+        Write_Bar(Bit_Of_Num(rate,2), '-', 2, '    ')
+        Write_Bar(Bit_Of_Num(rate,1), '-', 2, '')
+        print('\r')
+
--- v0.1.py
+++ v0.1.py
@@ -1,40 +1,13 @@
 # 通过密码的哈希算法产生的数列进行运算从而达到加密的效果
-import hashlib
-
-def The_Number_Of_Bit_Of_1 (StringOf16):
-    theDict = {"0":0,"1":1,"2":1,"3":2,"4":1,"5":2,"6":2,"7":3,\
-               "8":1,"9":2,"a":2,"b":3,"c":2,"d":3,"e":3,"f":4}
-    theNumber = 0
-    for i in StringOf16:
-        theNumber += theDict[i]
-    return theNumber
-
-def MixTwoByte (Byte1, Byte2):
-    return Byte1^Byte2
-
-def Mix_Two_Bytes (Bytes1, Bytes2):
-    MixedBytes = []
-    for i in range(Bytes1.__len__()):
-        MixedBytes.append (MixTwoByte (Bytes1[i], Bytes2[i]))
-    return bytes(MixedBytes)
-
-def Negate_of_Bytes (Bytes):
-    AimBytes = []
-    for Byte in Bytes:
-        AimBytes.append (~Byte)
-    return bytes(AimBytes)
-
-theMD5_of_Peterlits = \
-    b'\xa2\x44\xd2\xc5\x45\x70\x7d\x00\x84\x13\x82\x2a\xa3\x74\xcf\xab'
-theFiles_flag = 0
+import lib.SakurajimaLIB
 
 print ('''    ------------------------------------------------------------
     Hello, this Programma is used to Encrypt the File(Retain the
     Original File), which only need the Address and Password. 
     ------------------------------------------------------------''')
 
-theAddress = input("\nPlease Enter the Address of the File:")
-thePassword = input("Please Enter the Password:")
+theAddress = input("\n    Please Enter the Address of the File:")
+thePassword = input("    Please Enter the Password:")
 theOriginalFile = open(theAddress,'rb')
 theFile = open(theAddress + ".enced",'wb+')
 
@@ -45,8 +18,11 @@ theMD5_bytes = Mix_Two_Bytes(theMD5_bytes,theMD5_of_Peterlits)
 if The_Number_Of_Bit_Of_1(theMD5_bytes.hex()) <= 32:
     theMD5_bytes = Negate_of_Bytes (theMD5_bytes)
 
-print ("\nThe MD5 of the Password is " + theMD5_bytes.hex() +".")
-print ("Now It is Going to Work:\n")
+print ("\n    The MD5 of the Password is " + theMD5_bytes.hex() +".")
+print ("    Now It is Going to Work:\n")
+
+file_len = os.path.getsize(theAddress)
+bar_thread = threading.Thread(target = Bars,args = (file_flag, file_len))
 
 while True:
     OneAdd = Mix_Two_Bytes(theOriginalFile.read(16),theMD5_bytes)
@@ -56,4 +32,4 @@ while True:
 
 theOriginalFile.close()
 theFile.close()
-print ("Overed. The Encrypted Files is next to the Original File.")
+print ("    Overed. The Encrypted Files is next to the Original File.")
